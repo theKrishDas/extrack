24fedb4c67167a7643426422e3b877b20dd9f4c5
Krish Das <krishdasatgames@gmail.com>
2024-01-29 13:35:01 +0530

    ADD: Comment to switch to preview branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <>
-      <h1>Hello Next js</h1>
-    </>
+    <div className="flex h-[100dvh] flex-col items-start justify-center pl-3 md:items-center md:p-0">
+      <h1 className="text-4xl font-light">Main version has no releases</h1>
+      <p>Switch to Preview</p>
+    </div>
   );
 }
