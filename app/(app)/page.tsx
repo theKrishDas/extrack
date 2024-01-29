9949fdf657f35593b7c7319cf37d8055a394f0f8
Krish Das <krishdasatgames@gmail.com>
2024-01-29 18:30:32 +0530

    Install: Shadcn ui
    Just initiated shad-cn and install button component

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1>
         Welcome to <span className="font-mono font-semibold">PJO-24</span>
       </h1>
+      <Button>Click Me</Button>
     </>
   );
 }
