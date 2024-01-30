b669b8c61c87014178e4975e5a48a97d0c8d2a7b
Krish Das <krishdasatgames@gmail.com>
2024-01-30 10:57:42 +0530

    Fix issue where clerk makes all pages dynamic.
    Wraped the Provider component with a suspense boundary.

--- providers/Provider.tsx
+++ providers/Provider.tsx
@@ -1,9 +1,14 @@
+"use client";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Suspense } from "react";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ClerkProvider>{children}</ClerkProvider>
+      {/* //TODO: Add custom fallback component */}
+      <Suspense fallback={"Loading..."}>
+        <ClerkProvider>{children}</ClerkProvider>
+      </Suspense>
     </>
   );
 }
