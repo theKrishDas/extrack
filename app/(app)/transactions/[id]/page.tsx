0130b544a90beae7a1178b66f6ea39b80c870cef
Krish Das <krishdasatgames@gmail.com>
2024-02-01 19:54:20 +0530

    Feature: Implement Dynamic Transaction Details Page
    Added a dynamic page to render the details of each transaction. The committed code is a work in progress and currently displays only the transaction ID on the page. This serves as the initial stage of development for the transaction details page.
    
    Please note that further enhancements and additional details will be incorporated in subsequent commits as the development progresses.

---
+++ app/(app)/transactions/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { getTransactionById } from "@/actions/handle-transaction";
+import TransactionDetail from "@/components/transactions/TransactionDetail";
+import { notFound } from "next/navigation";
+
+export default async function TransactionDetailPage({
+  params: { id: _id },
+}: {
+  params: { id: string };
+}) {
+  const { transaction, error } = await getTransactionById(_id);
+
+  // TODO: Handle this error Properly
+  /**
+   * Check for error, undefined transaction, or empty transaction array.
+   * If any of these conditions is true, return a "not found" response.
+   * - `error` evaluates truthy when an error has occurred (ex: `.../transaction/invalid-uuid-format`).
+   * - `!transaction` is true when the transaction is undefined.
+   * - `transaction.length === 0` is true when the transaction array is empty.
+   */
+  if (error || !transaction || transaction.length === 0) return notFound();
+
+  return (
+    <>
+      <TransactionDetail transaction={transaction[0]} />
+    </>
+  );
+}
