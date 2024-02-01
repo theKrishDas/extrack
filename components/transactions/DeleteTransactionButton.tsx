1a9a8c1b47b8ec06ea3e5cc1bbd01d06b2c0f389
Krish Das <krishdasatgames@gmail.com>
2024-02-01 20:51:02 +0530

    Enhancement: Implement Loading Spinner for Transaction Deletion

--- components/transactions/DeleteTransactionButton.tsx
+++ components/transactions/DeleteTransactionButton.tsx
@@ -3,7 +3,9 @@ import { deleteTransaction } from "@/actions/handle-transaction";
 import { Button } from "@/components/ui/button";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
+import Spinner from "@/components/spinner";
 import { IoTrashBinOutline } from "react-icons/io5";
 
 export default function DeleteTransactionButton({
@@ -12,24 +14,28 @@ export default function DeleteTransactionButton({
   transactionId: string;
 }) {
   const router = useRouter();
+  const [isDeletingTransaction, setDeletingTransaction] = useState(false);
 
   return (
     <Button
       variant="destructive"
       size="icon"
       onClick={async () => {
+        setDeletingTransaction(true);
+
         const { error } = await deleteTransaction(transactionId);
+
+        setDeletingTransaction(false);
 
         // TODO: Handle error and success
         if (error) {
           console.error(error);
-          return;
+        } else {
+          router.push("/");
         }
-
-        router.push("/");
       }}
     >
-      <IoTrashBinOutline />
+      {isDeletingTransaction ? <Spinner size={14} /> : <IoTrashBinOutline />}
     </Button>
   );
 }
