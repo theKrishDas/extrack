2b09c689ea22fff735203de60d015531d602811d
Krish Das <krishdasatgames@gmail.com>
2024-02-03 11:46:42 +0530

    Refactor: Remove unused comment

--- app/(app)/transactions/[id]/page.tsx
+++ app/(app)/transactions/[id]/page.tsx
@@ -11,13 +11,6 @@ export default async function TransactionDetailPage({
 }) {
   const { transaction, error } = await getTransactionById(_id);
 
-  /**
-   * Check for error, undefined transaction, or empty transaction array.
-   * If any of these conditions is true, return a "not found" response.
-   * - `error` evaluates truthy when an error has occurred (ex: `.../transaction/invalid-uuid-format`).
-   * - `!transaction` is true when the transaction is undefined.
-   * - `transaction.length === 0` is true when the transaction array is empty.
-   */
   // TODO: Handle this error Properly
   if (error) return <p>{error}</p>;
 
