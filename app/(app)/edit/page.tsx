b41ce120ce0fe7a5c70492ec1835174cca7361f2
Krish Das <krishdasatgames@gmail.com>
2024-02-04 11:33:20 +0530

    Add: Form and Prefilled Data in /edit page
    Added a form to the /edit page and replaced the form values with
    preexisting data when the id query string parameter is present. As of
    this commit, the page allows users to view the form with prefilled data,
    but no database call has been implemented to update the transaction yet.

--- app/(app)/edit/page.tsx
+++ app/(app)/edit/page.tsx
@@ -1,7 +1,27 @@
-export default function AddTransactionPage({
+import { getTransactionById } from "@/actions/handle-transaction";
+import TransactionForm from "@/components/form/EditTransactionForm";
+import { notFound, redirect } from "next/navigation";
+
+export default async function AddTransactionPage({
   searchParams,
 }: {
   searchParams: { id: string };
 }) {
-  return <>{searchParams.id}</>;
+  const editId = searchParams.id;
+
+  if (!editId) redirect("/new");
+
+  const { transaction, error } = await getTransactionById(editId);
+
+  // TODO: Handle this error Properly
+  if (error) return <p>{error}</p>;
+
+  if (!transaction) return notFound();
+
+  return (
+    <>
+      <h1>Add New transaction</h1>
+      <TransactionForm initialTransactionData={transaction} />
+    </>
+  );
 }
