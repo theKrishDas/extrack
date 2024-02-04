5fe8a6406f1471f23c4944711e0f29cd5386e477
Krish Das <krishdasatgames@gmail.com>
2024-02-04 19:42:25 +0530

    Refactor: Implement Zod Input Validation for Amount Field
    Introduced Zod input validation for the amount field to ensure that the minimum amount value is at least 0.01.

--- lib/form-schema/new-transaction-schema.ts
+++ lib/form-schema/new-transaction-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const NewTransactionSchema = z.object({
-  amount: z.number(),
+  amount: z.number().min(0.01, { message: "Amount must be at least 0.01" }),
   label: z.string().optional(),
   // category: z.string().nullable().optional(), // TODO: Add later
   is_expense: z.boolean(),
