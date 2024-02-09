2d3ec097e0edaafcb9d9c094002a17ab8b029d45
Krish Das <krishdasatgames@gmail.com>
2024-02-09 20:22:56 +0530

    Feature: Implement Utility Function for Formatted Currency Calculation
    Added a utility function to calculate formatted currency based on the locale.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+const LOCALE = "en-IN";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -9,4 +11,17 @@ export function wait(milliseconds: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, milliseconds);
   });
+}
+
+export function formatCurrency(amount: number) {
+  const format = new Intl.NumberFormat(LOCALE, {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  });
+
+  const formattedCurrency = format.format(amount);
+
+  return formattedCurrency;
 }
