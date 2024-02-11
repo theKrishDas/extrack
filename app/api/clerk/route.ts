a822405e12a553e6b5f1ff1b2e679b38c1e3857d
Krish Das <krishdasatgames@gmail.com>
2024-02-11 20:33:34 +0530

    Feature: Implement Webhook for Clerk User Creation
    When a new user is created in Clerk, a new row containing the user_id will be automatically added to the preferences table.

--- app/api/clerk/route.ts
+++ app/api/clerk/route.ts
@@ -1,10 +1,22 @@
+import { db } from "@/db";
+import { preference } from "@/db/drizzle/schema";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const payload: WebhookEvent = await request.json();
 
-  console.log(payload);
+  const payloadId = payload.data.id;
+
+  if (!payloadId)
+    return NextResponse.json(
+      { error: "An unexpected error occured while creating new profile" },
+      { status: 400 },
+    );
+
+  console.log(payloadId);
+
+  await db.insert(preference).values({ userId: payloadId });
   return NextResponse.json({ success: "Profile created" }, { status: 201 });
 }
 
