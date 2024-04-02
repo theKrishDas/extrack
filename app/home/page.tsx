446c0d41347f0fffbbe56be16eb4e89ef5f046d6
Krish-Das <krishdasatgames@gmail.com>
2024-04-02 23:21:10 +0530

    Feat: Style and Enhance Home Page
    Styled the `/home` page to improve its visual appearance and user experience. Added a button that redirects users to the sign-in page and included a heading to provide context and improve page layout.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,11 +3,16 @@ import { SignInButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
-    <div>
-      <p>This should be accessed by everyone</p>
-      <Button size="lg" variant="default" asChild>
-        <SignInButton />
-      </Button>
-    </div>
+    <main className="container flex h-[100dvh] flex-col items-center">
+      <section className="flex w-full flex-1 items-center justify-center">
+        <h1 className="text-xl font-bold tracking-tight">Extrack.</h1>
+      </section>
+
+      <div className="flex h-24 w-full items-center justify-center">
+        <Button size="lg" className="h-14 w-full rounded-2xl text-base" asChild>
+          <SignInButton />
+        </Button>
+      </div>
+    </main>
   );
 }
