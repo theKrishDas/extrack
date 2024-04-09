8b427b39f211a539affa538f72a8d89808c3eb59
Krish-Das <krishdasatgames@gmail.com>
2024-04-09 19:59:26 +0530

    Feat: Implement edit functionality for initial balance
    Added functionality to the Edit Initial Balance component so that clicking the edit button replaces the balance text with an input field that auto-focuses. When the input field loses focus, it reverts to displaying the balance text, discarding any changes made to the input value, effectively canceling the editing of the initial balance.
    
    Note: Database is not connectied with this component, meaning editing
    the initial component does nothing.

--- components/settings/InitialBalanceSettings.tsx
+++ components/settings/InitialBalanceSettings.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { IoBag } from "react-icons/io5";
 import { RxPencil1 } from "react-icons/rx";
+import CurrencyInput from "react-currency-input-field";
+import { useState } from "react";
 
 export default function InitialBalanceSettings({
   initialBalance,
 }: {
   initialBalance: number;
 }) {
+  const [isEditingInitialBalance, setIsEditingInitialBalance] = useState(false);
+
   return (
     <div className="flex flex-col gap-7 rounded-3xl bg-card p-5">
       {/* Header */}
@@ -25,14 +31,39 @@ export default function InitialBalanceSettings({
           className="h-12 w-12 rounded-full text-lg"
           variant="ghost"
           size="icon"
+          disabled={isEditingInitialBalance}
+          onClick={() => {
+            setIsEditingInitialBalance(true);
+          }}
         >
           <RxPencil1 />
         </Button>
       </div>
-      <p className="inline-flex items-start text-3xl tracking-tight text-foreground/70">
-        <span className="pr-1 text-xl font-light text-foreground/40">₹</span>
-        {initialBalance}
-      </p>
+      {isEditingInitialBalance ? (
+        <TheInput setIsEditingInitialBalance={setIsEditingInitialBalance} />
+      ) : (
+        <p className="inline-flex items-start text-3xl tracking-tight text-foreground/70">
+          <span className="pr-1 text-xl font-light text-foreground/40">₹</span>
+          {initialBalance}
+        </p>
+      )}
     </div>
+  );
+}
+
+function TheInput({
+  setIsEditingInitialBalance,
+}: {
+  setIsEditingInitialBalance: (_: boolean) => void;
+}) {
+  return (
+    <CurrencyInput
+      autoFocus
+      decimalsLimit={2}
+      allowNegativeValue={false}
+      maxLength={8}
+      placeholder="New Balance"
+      onBlur={() => setIsEditingInitialBalance(false)}
+    />
   );
 }
