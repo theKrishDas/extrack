3f0cf31eb9b986991b2190f17fa0ffa666f6236f
Krish-Das <krishdasatgames@gmail.com>
2024-04-10 12:45:07 +0530

    Feature: Add Save Buttons for Edit Initial Balance
    Added save and cancel buttons to the Edit Initial Balance component to save or cancel the edit of the dirty input value when editing the initial balance.
    
    Note: that this feature is not yet implemented, and the buttons do not currently work.

--- components/settings/InitialBalanceSettings.tsx
+++ components/settings/InitialBalanceSettings.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { IoBag } from "react-icons/io5";
+import { IoBag, IoCloseSharp, IoCheckmarkSharp } from "react-icons/io5";
 import { RxPencil1 } from "react-icons/rx";
 import CurrencyInput from "react-currency-input-field";
 import { useState } from "react";
@@ -12,6 +12,7 @@ export default function InitialBalanceSettings({
   initialBalance: number;
 }) {
   const [isEditingInitialBalance, setIsEditingInitialBalance] = useState(false);
+  const [isInputDirty, setIsInputDirty] = useState(false);
 
   const handleEditStart = () => {
     setIsEditingInitialBalance(true);
@@ -25,7 +26,7 @@ export default function InitialBalanceSettings({
   };
 
   return (
-    <div className="flex flex-col gap-7 rounded-3xl bg-card p-5">
+    <section className="flex flex-col gap-7 rounded-3xl bg-card p-5">
       {/* Header */}
       <div className="inline-flex w-full items-center justify-between">
         {/* IDK */}
@@ -37,26 +38,48 @@ export default function InitialBalanceSettings({
           <h4 className="text-lg">Initial balance</h4>
         </div>
 
-        {/* Icon */}
+        {/* Edit Button */}
         <Button
           className="h-12 w-12 rounded-full text-lg"
           variant="ghost"
           size="icon"
-          disabled={isEditingInitialBalance}
+          // disabled={isEditingInitialBalance}
           onClick={() => handleEditStart()}
         >
           <RxPencil1 />
         </Button>
       </div>
-      {isEditingInitialBalance ? (
-        <TheInput handleEditEnd={handleEditEnd} />
-      ) : (
-        <p className="inline-flex items-start text-3xl tracking-tight text-foreground/70">
-          <span className="pr-1 text-xl font-light text-foreground/40">₹</span>
-          {initialBalance}
-        </p>
-      )}
-    </div>
+
+      <div className="flex items-center justify-between">
+        {/* Input / display */}
+        {isEditingInitialBalance ? (
+          <TheInput handleEditEnd={handleEditEnd} />
+        ) : (
+          <p className="inline-flex w-full items-start text-3xl tracking-tight text-foreground/70">
+            <span className="pr-1 text-xl font-light text-foreground/40">
+              ₹
+            </span>
+            {initialBalance}
+          </p>
+        )}
+
+        {/* Save bttons */}
+        {isInputDirty ? (
+          <div className="flex flex-row-reverse gap-1">
+            <Button size="icon" className="rounded-full text-lg">
+              <IoCheckmarkSharp />
+            </Button>
+            <Button
+              size="icon"
+              variant="ghost"
+              className="rounded-full text-lg"
+            >
+              <IoCloseSharp />
+            </Button>
+          </div>
+        ) : null}
+      </div>
+    </section>
   );
 }
 
@@ -68,6 +91,7 @@ function TheInput({ handleEditEnd }: { handleEditEnd: () => void }) {
       allowNegativeValue={false}
       maxLength={8}
       placeholder="New Balance"
+      className="w-full"
       onBlur={() => handleEditEnd()}
     />
   );
