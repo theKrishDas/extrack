75409afe8ee7dbf736227696e9c06b062d89192e
Krish-Das <krishdasatgames@gmail.com>
2024-04-19 13:37:50 +0530

    Add Form

---
+++ app/(app)/settings/TestInitialBalanceSettings.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+type FormSchema = {
+  amount: number;
+};
+
+export default function TestInitialBalanceSettings() {
+  const form = useForm<FormSchema>({
+    defaultValues: {
+      amount: 0,
+    },
+  });
+
+  function onSubmit(values: FormSchema) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Starting balance</FormLabel>
+                <FormControl>
+                  <Input placeholder="Amount" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your balance at the time of first using this app.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
+}
