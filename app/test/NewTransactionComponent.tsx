27fa76da4c8e41dd0a845858683baf7a8b579d8f
Krish-Das <krishdasatgames@gmail.com>
2024-04-20 15:54:15 +0530

    Feat: Add dynamic colors to the tab
    Tab color is now dynamically changed based on the tab type

--- app/test/NewTransactionComponent.tsx
+++ app/test/NewTransactionComponent.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { IoAddSharp, IoRemoveSharp } from "react-icons/io5";
 import { TCategories, TTransactionType } from "./page";
 import SelectComponent from "./SelectComponent";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function NewTransactionComponent({
   incomeCategories,
@@ -10,9 +14,17 @@ export default function NewTransactionComponent({
   incomeCategories: TCategories[];
   expenseCategories: TCategories[];
 }) {
+  const DEFAULT_ACTIVE_TAB = "expense";
+  const [activeTab, setActiveTab] =
+    useState<TTransactionType>(DEFAULT_ACTIVE_TAB);
+
   return (
     <>
-      <Tabs defaultValue="expense">
+      <Tabs
+        defaultValue={DEFAULT_ACTIVE_TAB}
+        value={activeTab}
+        onValueChange={(value) => setActiveTab(value as TTransactionType)}
+      >
         <TabsList className="h-fit w-fit rounded-full bg-card">
           <TransactionTypeSwitchTrigger type="income" />
           <TransactionTypeSwitchTrigger type="expense" />
@@ -33,7 +45,15 @@ function TransactionTypeSwitchTrigger({ type }: { type: TTransactionType }) {
   const Icon = () => (type === "income" ? <IoAddSharp /> : <IoRemoveSharp />);
 
   return (
-    <TabsTrigger value={type} className="h-10 w-12 rounded-full text-base">
+    <TabsTrigger
+      value={type}
+      className={cn(
+        "h-8 w-12 rounded-full text-base",
+        type === "income"
+          ? "data-[state=active]:bg-success data-[state=active]:text-success-foreground"
+          : "data-[state=active]:bg-destructive data-[state=active]:text-destructive-foreground",
+      )}
+    >
       <Icon />
     </TabsTrigger>
   );
