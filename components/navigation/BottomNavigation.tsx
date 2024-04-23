633f6bc2bf1c51931b90cd35a885acd83dcc2a43
Krish-Das <krishdasatgames@gmail.com>
2024-04-23 21:41:58 +0530

    Refactor: Replace link with button in bottom-navigation
    The link to "/new" in the bottom-navigation has been replaced with a button that triggers the opening of the new transaction drawer.

--- components/navigation/BottomNavigation.tsx
+++ components/navigation/BottomNavigation.tsx
@@ -1,42 +1,32 @@
-import type { BottomNavContentType } from "@/lib/types/bottom-nav-content";
 import { GoHome, GoHomeFill } from "react-icons/go";
 import { IoSettingsOutline, IoSettingsSharp } from "react-icons/io5";
-import { TbSquareRoundedPlus, TbSquareRoundedPlusFilled } from "react-icons/tb";
 import BottomNavLink from "./BottonNavLink";
 import NewTransactionDrawer from "@/app/test/NewTransactionDrawer";
-
-const navContent: BottomNavContentType[] = [
-  {
-    name: "Home",
-    href: "/",
-    activeIcon: <GoHomeFill />,
-    inActiveIcon: <GoHome />,
-  },
-  {
-    name: "",
-    href: "/new",
-    activeIcon: <TbSquareRoundedPlusFilled className="text-4xl" />,
-    inActiveIcon: (
-      <TbSquareRoundedPlus className="text-4xl" strokeWidth={1.3} />
-    ),
-  },
-  {
-    name: "Settings",
-    href: "/settings",
-    activeIcon: <IoSettingsSharp />,
-    inActiveIcon: <IoSettingsOutline />,
-  },
-];
 
 export default function BottomNavigation() {
   return (
     // NOTE: --- bottom navigation has z-index: 9 ---
     <footer className="fixed bottom-0 left-0 isolate z-[9] w-full border-t bg-background/80 backdrop-blur-md">
       <nav className="container flex items-center justify-around py-2">
-        {navContent.map((content) => (
-          <BottomNavLink key={content.href} prop={content} />
-        ))}
+        <BottomNavLink
+          prop={{
+            name: "Home",
+            href: "/",
+            activeIcon: <GoHomeFill />,
+            inActiveIcon: <GoHome />,
+          }}
+        />
+
         <NewTransactionDrawer />
+
+        <BottomNavLink
+          prop={{
+            name: "Settings",
+            href: "/settings",
+            activeIcon: <IoSettingsSharp />,
+            inActiveIcon: <IoSettingsOutline />,
+          }}
+        />
       </nav>
     </footer>
   );
