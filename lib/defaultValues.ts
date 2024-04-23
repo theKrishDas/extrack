cb6489e3d047a1b240567c1eee983978e74f0a4e
Krish-Das <krishdasatgames@gmail.com>
2024-04-23 20:53:51 +0530

    Add: More default values
    Add new default values: `MAX_AMOUNT_LIMIT` and `DEFAULT_TRANSACTION_IS_EXPENSE`.

--- lib/defaultValues.ts
+++ lib/defaultValues.ts
@@ -3,4 +3,8 @@
 //
 export const MINIMUM_SARTING_BALANCE = 3_000 as const;
 // Default tab when adding new transaction
-export const DEFAULT_ACTIVE_TAB = "expense" as const;
+export const DEFAULT_ACTIVE_TAB = "expense" as const; // expense | income
+
+export const MAX_AMOUNT_LIMIT = 1_00_00_000 as const;
+export const DEFAULT_TRANSACTION_IS_EXPENSE =
+  DEFAULT_ACTIVE_TAB === ("expense" as const);
