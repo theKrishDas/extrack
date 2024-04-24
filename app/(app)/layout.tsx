29feb938dbf8f0305c0a509d57cf7df62dfa628c
Krish-Das <krishdasatgames@gmail.com>
2024-04-24 22:47:21 +0530

    Fix: Add spacing at the bottom of the app
    Added spacing to the bottom of the app to prevent content from being obscured by the fixed bottom-navigation.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -5,6 +5,8 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <>
       <main className="container space-y-8 py-4">{children}</main>
 
+      {/* NOTE: This spacing is to compensate the bottom navigation */}
+      <div className="h-16 w-full" />
       <BottomNavigation />
     </>
   );
