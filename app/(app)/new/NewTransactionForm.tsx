3214cdbf59c76a02a26a99e37c39b5cf7dc035ec
Krish-Das <krishdasatgames@gmail.com>
2024-04-24 16:30:47 +0530

    Modify: Wrap the component with form

--- app/(app)/new/NewTransactionForm.tsx
+++ app/(app)/new/NewTransactionForm.tsx
@@ -7,6 +7,20 @@ import CurrencyInput from "react-currency-input-field";
 import { TTransactionType } from "./NewTransactionTab";
 import { PickDate } from "./PickDate";
 import SelectCategory from "./SelectCategory";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+
+type TFormData = {
+  label: string;
+  amount: number;
+};
 
 export default function NewTransactionForm({
   tabType,
@@ -15,59 +29,63 @@ export default function NewTransactionForm({
 }) {
   const isExpense = tabType !== "income";
 
+  const form = useForm<TFormData>({ defaultValues: { label: "" } });
+
   return (
-    <div className="space-y-2">
-      <div className="inline-flex items-center gap-2">
-        <PickDate />
-        <SelectCategory />
-      </div>
-
-      {/* --- --- INPUTS --- --- */}
-      <section className="flex w-full flex-col items-start justify-center gap-8 rounded-3xl bg-card p-10">
-        <div className="justify-betweens inline-flex w-full items-center">
-          <input
-            className="min-w-0 flex-1 bg-transparent text-lg outline-none"
-            placeholder="Add a Label"
-          />
-
-          <IoArrowUpSharp
-            size={16}
-            className={cn(
-              "rotate-0 text-success transition-all",
-              isExpense && "rotate-180 text-destructive",
-            )}
-          />
+    <Form {...form}>
+      <form className="space-y-2">
+        <div className="inline-flex items-center gap-2">
+          <PickDate />
+          <SelectCategory />
         </div>
 
-        <CurrencyInput
-          decimalsLimit={2}
-          allowNegativeValue={false}
-          maxLength={8}
-          placeholder="Amount"
-          className={cn(
-            "w-full truncate border-none bg-transparent text-5xl font-light tracking-tight outline-none",
-            isExpense ? "text-destructive" : "text-success",
-          )}
-        />
-      </section>
+        {/* --- --- INPUTS --- --- */}
+        <section className="flex w-full flex-col items-start justify-center gap-8 rounded-3xl bg-card p-10">
+          <div className="justify-betweens inline-flex w-full items-center">
+            <input
+              className="min-w-0 flex-1 bg-transparent text-lg outline-none"
+              placeholder="Add a Label"
+            />
 
-      <div className="flex flex-row-reverse items-center gap-2">
-        <Button
-          variant="secondary"
-          className={cn(
-            "h-14 w-full rounded-full bg-card text-base shadow-none",
-            isExpense ? "text-destructive" : "text-success",
-          )}
-        >
-          Add
-        </Button>
-        <Button
-          className="h-14 w-full rounded-full text-base text-foreground/50"
-          variant="ghost"
-        >
-          Cancel
-        </Button>
-      </div>
-    </div>
+            <IoArrowUpSharp
+              size={16}
+              className={cn(
+                "rotate-0 text-success transition-all",
+                isExpense && "rotate-180 text-destructive",
+              )}
+            />
+          </div>
+
+          <CurrencyInput
+            decimalsLimit={2}
+            allowNegativeValue={false}
+            maxLength={8}
+            placeholder="Amount"
+            className={cn(
+              "w-full truncate border-none bg-transparent text-5xl font-light tracking-tight outline-none",
+              isExpense ? "text-destructive" : "text-success",
+            )}
+          />
+        </section>
+
+        <div className="flex flex-row-reverse items-center gap-2">
+          <Button
+            variant="secondary"
+            className={cn(
+              "h-14 w-full rounded-full bg-card text-base shadow-none",
+              isExpense ? "text-destructive" : "text-success",
+            )}
+          >
+            Add
+          </Button>
+          <Button
+            className="h-14 w-full rounded-full text-base text-foreground/50"
+            variant="ghost"
+          >
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 }
