edbe4cf88d51b3ab20614d40d3cacb2b3b021022
Krish-Das <krishdasatgames@gmail.com>
2024-04-25 17:07:39 +0530

    Feat: Add skeleton loading in new transaction page

--- app/(app)/new/page.tsx
+++ app/(app)/new/page.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
 import PageTitleWithNavigation from "@/components/navigation/PageTitleWithNavigation";
 import NewTransactionTab from "./NewTransactionTab";
+import NewTransactionFallback from "./NewTransactionFallback";
 
 export default function NewTransactionPage() {
   return (
     <>
       <PageTitleWithNavigation heading="Add new" href="/" />
-      <NewTransactionTab />
+
+      <Suspense fallback={<NewTransactionFallback />}>
+        <NewTransactionTab />
+      </Suspense>
     </>
   );
 }
