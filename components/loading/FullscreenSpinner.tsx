bfe00c1e452613ff53cf7b0973826298c543be28
Krish-Das <krishdasatgames@gmail.com>
2024-04-29 19:50:48 +0530

    Fix: z-index on full-screen loading spinner

--- components/loading/FullscreenSpinner.tsx
+++ components/loading/FullscreenSpinner.tsx
@@ -2,8 +2,9 @@ import Spinner from "@/components/spinner";
 
 export default function FullscreenSpinner() {
   return (
+    // NOTE: --- bottom navigation has z-index: 9 ---
     // TODO: Fix layout shifts because of this component and spacing of main element
-    <div className="pointer-events-none absolute inset-0 left-0 top-0 z-[998] grid place-content-center bg-background">
+    <div className="pointer-events-none absolute inset-0 left-0 top-0 isolate z-[8] grid place-content-center bg-background">
       <Spinner />
     </div>
   );
