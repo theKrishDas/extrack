fa839e366b82e78346d7b44d6822c115c72b2141
Krish-Das <krishdasatgames@gmail.com>
2024-09-27 23:16:23 +0530

    Feature: Configured /edit-v2 page to always be dynamically rendered
    Added `revalidate` and `dynamic` options to the `/edit-v2` page to ensure that it is always dynamically rendered.
    This change resolves the issue of rendering outdated data about the transaction when editing the transaction.

--- app/(app)/edit-v2/[id]/page.tsx
+++ app/(app)/edit-v2/[id]/page.tsx
@@ -4,6 +4,9 @@ import AppNavigateBack from "@/components/navigation/AppNavigateBack";
 import { IoCloseSharp } from "react-icons/io5";
 import { notFound } from "next/navigation";
 
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+
 export default async function EditTransactionPage({
   params: { id: _id },
 }: {
