37bfdc37f59315f1541c32339b4dffa3816cdd25
Krish-Das <krishdasatgames@gmail.com>
2024-09-27 21:13:23 +0530

    Feature: Add new page for editing transactions (/edit-v2)
    Created a new page `/edit-v2` to edit pre-existing transactions.
    This page is currently under development and will replace the existing `/edit` page in the future.

---
+++ app/(app)/edit-v2/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { getTransactionById } from "@/actions/handle-transaction";
+import TransactionForm from "@/components/form/EditTransactionForm";
+import AppNavigateBack from "@/components/navigation/AppNavigateBack";
+import { IoCloseSharp } from "react-icons/io5";
+import { notFound } from "next/navigation";
+
+export default async function EditTransactionPage({
+  params: { id: _id },
+}: {
+    params: { id: string };
+  }) {
+
+  const { transaction, error } = await getTransactionById(_id);
+
+  // TODO: Handle this error Properly
+  if (error || !transaction) return notFound();
+
+  return <>
+    <AppNavigateBack heading="Edit" icon={<IoCloseSharp />} />
+    <TransactionForm initialTransactionData={transaction} />
+  </>
+}
---
+++ app/(app)/edit-v2/layout.tsx
@@ -0,0 +1,9 @@
+import { Main } from "@/components/mainwrapper";
+
+export default function EditTransactionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Main>{children}</Main>;
+}
