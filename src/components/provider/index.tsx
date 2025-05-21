bc509b090ff4b1fb061cd63d0f3cfebb882f1e07
Krish-Das <krishdasatgames@gmail.com>
2025-05-21 20:58:39 +0530

    feat: add clerk to the project

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -1,3 +1,11 @@
+"use client"
+
+import { ClerkProvider } from "@clerk/nextjs"
+
 export default function Provider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return (
+    <>
+      <ClerkProvider>{children}</ClerkProvider>
+    </>
+  )
 }
