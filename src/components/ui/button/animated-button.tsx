e60f7865ad504827ec42425cc3b6ada6fd1ba3fa
Krish-Das <krishdasatgames@gmail.com>
2025-05-21 11:00:52 +0530

    feat: first draft of the button
    Add the button animations

--- src/components/ui/button/animated-button.tsx
+++ src/components/ui/button/animated-button.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import {RefObject} from "react"
+import {mergeRefs} from "@react-aria/utils"
+import {useAnimate} from "motion/react-mini"
 import {
   Button as RacButton,
   type ButtonProps as RacButtonProps,
@@ -13,13 +15,29 @@ interface ButtonProps extends RacButtonProps {
 }
 
 const Button = ({className, ref, ...rest}: ButtonProps) => {
+  const [scope, animate] = useAnimate()
+
+  const animatePressStart = (): void => {
+    animate(
+      scope.current,
+      {backgroundColor: "var(--button-highlight)"},
+      {duration: 0}
+    )
+  }
+  const animatePressEnd = (): void => {
+    animate(scope.current, {backgroundColor: "var(--button-bg)"})
+  }
+
   return (
     <RacButton
       className={cn(
         "border-separator-non-opaque rounded-2xl border p-3",
+        "[--button-bg:var(--fill-opaque)] [--button-highlight:var(--ios-blue)]",
         className
       )}
-      ref={ref}
+      onPressStart={animatePressStart}
+      onPressEnd={animatePressEnd}
+      ref={mergeRefs(ref, scope)}
       {...rest}
     />
   )
