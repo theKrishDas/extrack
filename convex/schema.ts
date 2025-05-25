c8bd0459ba760a428ac864dd3c7a38be75a0ba3c
Krish-Das <krishdasatgames@gmail.com>
2025-05-25 16:42:26 +0530

    fix: make note optional

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,7 +4,7 @@ import {v} from "convex/values"
 export default defineSchema({
   transactions: defineTable({
     amount: v.number(),
-    note: v.string(),
+    note: v.optional(v.string()),
     type: v.union(v.literal("expense"), v.literal("income")),
     // ownerId
     // category
