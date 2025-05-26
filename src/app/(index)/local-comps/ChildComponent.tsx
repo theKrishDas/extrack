b18489ea26727908dd6c5613e25200b0dfba9945
Krish-Das <krishdasatgames@gmail.com>
2025-05-26 15:18:13 +0530

    feat: use the new custom drawer component

--- src/app/(index)/local-comps/ChildComponent.tsx
+++ src/app/(index)/local-comps/ChildComponent.tsx
@@ -1,10 +1,17 @@
 "use client"
 
 import {useState} from "react"
-import {Drawer} from "vaul"
 
-import {cn} from "@/lib/utils"
 import {Button} from "@/components/ui/button/animated-button"
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 
 import InputComponent from "./InputComponent"
 
@@ -12,32 +19,25 @@ export default function ChildComponent() {
   const [open, setOpen] = useState<boolean>(false)
 
   return (
-    <Drawer.Root
-      shouldScaleBackground
-      setBackgroundColorOnScale={false}
-      open={open}
-      onOpenChange={setOpen}
-    >
-      <Drawer.Trigger asChild>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
         <Button className="backdrop-blur-md" color="gray">
           + Add
         </Button>
-      </Drawer.Trigger>
-      <Drawer.Portal>
-        <Drawer.Overlay className="bg-background/60 fixed inset-0" />
-        <Drawer.Content className="fixed right-0 bottom-1.5 left-0 h-fit px-1.5 outline-none [--initial-transform:calc(100%+0.375rem)]">
-          <Drawer.Title className="sr-only">Add new transaction</Drawer.Title>
-          <div
-            className={cn(
-              "rounded-2xl p-4 before:rounded-2xl",
-              // Material
-              "bg-fill-quaternary shadow-[inset_0_1px,inset_0_0_0_1px] shadow-white/[0.025] backdrop-blur-2xl"
-            )}
-          >
-            <InputComponent setOpen={setOpen} />
-          </div>
-        </Drawer.Content>
-      </Drawer.Portal>
-    </Drawer.Root>
+      </DrawerTrigger>
+
+      <DrawerContent>
+        <DrawerHeader className="sr-only">
+          <DrawerTitle>Add a new transaction.</DrawerTitle>
+          <DrawerDescription>
+            Fill up this form to add new transaction.
+          </DrawerDescription>
+        </DrawerHeader>
+
+        <DrawerBody>
+          <InputComponent setOpen={setOpen} />
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
   )
 }
