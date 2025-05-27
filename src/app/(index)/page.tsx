a27e7a3a20207012cfa61264a91cd8643e11b146
Krish-Das <krishdasatgames@gmail.com>
2025-05-27 22:05:30 +0530

    feat: add the dock

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,4 +1,5 @@
 import {Button} from "@/components/ui/button/animated-button"
+import Dock from "@/components/app/Dock"
 import Summary from "@/components/app/Summary"
 import {IonChevronForward} from "@/components/icons/ion"
 import {Container} from "@/components/layout/container"
@@ -9,11 +10,14 @@ import Transactions from "./local-comps/Transactions"
 export default function Home() {
   return (
     <>
+      {/* // TODO: Get rid of this */}
       <nav className="fixed inset-x-0 bottom-1/5 inline-flex items-center justify-center">
         <div className="inline-flex w-full max-w-md justify-center">
           <ChildComponent />
         </div>
       </nav>
+
+      <Dock />
 
       {/* // WARN: this main container has a height */}
       <main className="min-h-dvh px-4 pt-6" data-vaul-drawer-wrapper="">
---
+++ src/components/app/Dock.tsx
@@ -0,0 +1,36 @@
+import {cn} from "@/lib/utils"
+
+import {IonPencil} from "../icons/ion"
+import {Button} from "../ui/button"
+
+export default function Dock() {
+  return (
+    <nav className="fixed inset-x-0 bottom-1.5 z-40 flex h-fit items-center justify-center gap-1 pb-6">
+      <div
+        className={cn(
+          "inline-flex items-center rounded-full",
+          "bg-fill-quaternary shadow-[inset_0_1px,inset_0_0_0_1px] shadow-white/[0.025] backdrop-blur-[8px]" // Material
+          // "bg-fill-quaternary shadow-[inset_0_1px,inset_0_0_0_1px] shadow-white/[0.025] backdrop-blur-2xl"
+        )}
+      >
+        {Array.from({length: 3}, (_, idx) => (
+          <Button key={idx} variant="ghost" color="gray" size="lg" isIconOnly>
+            <IonPencil />
+          </Button>
+        ))}
+      </div>
+
+      <div
+        className={cn(
+          "rounded-full",
+          "bg-fill-quaternary shadow-[inset_0_1px,inset_0_0_0_1px] shadow-white/[0.025] backdrop-blur-[8px]" // Material
+          // "bg-fill-quaternary shadow-[inset_0_1px,inset_0_0_0_1px] shadow-white/[0.025] backdrop-blur-2xl"
+        )}
+      >
+        <Button variant="tinted" color="blue" size="lg" isIconOnly>
+          <IonPencil />
+        </Button>
+      </div>
+    </nav>
+  )
+}
