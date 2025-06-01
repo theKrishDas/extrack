10852cc63ac266f4f8b1366365e73a907bb2ca7b
Krish-Das <krishdasatgames@gmail.com>
2025-06-01 18:00:01 +0530

    style(dock): add material to the dock

--- src/components/app/dock/src/Dock.tsx
+++ src/components/app/dock/src/Dock.tsx
@@ -2,6 +2,8 @@
 
 import {useState} from "react"
 
+import {Material} from "@/components/material/material"
+
 import Fab from "./Fab"
 import Island from "./Island"
 
@@ -10,9 +12,15 @@ const Dock = () => {
 
   return (
     <>
-      <nav className="fixed inset-x-0 bottom-1.5 z-40 flex items-center justify-center gap-1 pb-6">
-        <Island open={open} setOpen={setOpen} />
-        <Fab open={open} setOpen={setOpen} />
+      <nav className="fixed inset-x-0 bottom-4 z-40">
+        <Material
+          as="section"
+          className="mx-auto flex w-fit items-center justify-center gap-1 rounded-full p-1"
+          withBorder
+        >
+          <Island open={open} setOpen={setOpen} />
+          <Fab open={open} setOpen={setOpen} />
+        </Material>
       </nav>
     </>
   )
