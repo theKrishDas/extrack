c808754b80233b8c61e9e9717f476727cd9fb6f2
Krish-Das <krishdasatgames@gmail.com>
2025-06-02 12:40:52 +0530

    feat: add list component

---
+++ src/components/ui/list.tsx
@@ -0,0 +1,139 @@
+import {ComponentProps, CSSProperties} from "react"
+
+import {cn} from "@/lib/utils"
+
+const Root = ({className, ...rest}: ComponentProps<"ul">) => {
+  return (
+    <ul
+      className={cn(
+        "ListRoot",
+        "[&:has(+.ListRoot)]:mb-8.5",
+        // "[&:not(:has(.ListFooter))]:ring",
+        "[&:has(.ListFooter):has(~_ul.ListRoot_.ListHeading)]:mb-5.5",
+        "[&:not(:has(.ListFooter)):has(~_ul_.ListHeading)]:mb-8",
+        "[&:has(.ListFooter):not(:has(~_ul.ListRoot_.ListHeading))]:mb-10",
+        "last-of-type:[&:has(.ListFooter)]:mb-10 last-of-type:[&:not(:has(.ListFooter))]:mb-12.5",
+        className
+      )}
+      {...rest}
+    />
+  )
+}
+
+function Item({className, ...rest}: ComponentProps<"li">) {
+  return (
+    <li
+      className={cn(
+        "ListItem",
+        "bg-fill-quaternary flex h-12 items-center gap-2 pl-4 first-of-type:rounded-t-[0.95rem] last-of-type:rounded-b-[0.95rem]",
+        "last-of-type:[&_.ListContent]:border-none",
+        className
+      )}
+      {...rest}
+    />
+  )
+}
+
+function Content({className, ...rest}: ComponentProps<"div">) {
+  return (
+    <div
+      className={cn(
+        "ListContent border-separator-non-opaque inline-flex h-full w-full flex-1 items-center justify-between gap-2 truncate border-b-1 pr-4",
+        className
+      )}
+      {...rest}
+    />
+  )
+}
+
+function Icon({className, ...rest}: ComponentProps<"div">) {
+  return (
+    <div className={cn("ListIcon", "-ml-0.5 text-lg", className)} {...rest} />
+  )
+}
+
+function Text({
+  className,
+  level = "2",
+  fullWidth = false,
+  maskOverflow = false,
+  style,
+  align = "left",
+  ...rest
+}: ComponentProps<"p"> & {
+  level?: "1" | "2" | "3"
+  fullWidth?: boolean
+  maskOverflow?: boolean
+  align?: "left" | "center" | "right"
+}) {
+  const maskStyles: CSSProperties | undefined = maskOverflow
+    ? {
+        WebkitMaskImage: `linear-gradient(to ${align === "left" ? "right" : "left"}, black 80%, transparent 100%)`,
+        maskImage: `linear-gradient(to ${align === "left" ? "right" : "left"}, black 80%, transparent 100%)`,
+        WebkitMaskRepeat: "no-repeat",
+        maskRepeat: "no-repeat",
+      }
+    : undefined
+
+  return (
+    <p
+      className={cn(
+        "ListText",
+        "-tracking-[0.01em]",
+        level === "1" && "text-label font-semibold",
+        level === "2" && "text-label-secondary font-medium",
+        level === "3" && "text-label-tertiary",
+        align === "left" && "text-left",
+        align === "center" && "text-center",
+        align === "right" && "text-right",
+        maskOverflow && "overflow-hidden whitespace-nowrap",
+        fullWidth && "w-full max-w-full flex-1",
+        className
+      )}
+      style={{
+        ...maskStyles,
+        ...style,
+      }}
+      {...rest}
+    />
+  )
+}
+
+function Heading({
+  className,
+  srOnly = false,
+  ...rest
+}: ComponentProps<"h4"> & {srOnly?: boolean}) {
+  return (
+    <h4
+      className={cn(
+        "ListHeading",
+        "text-label-tertiary px-4 pb-1.5 text-sm font-medium uppercase",
+        srOnly && "sr-only",
+        className
+      )}
+      {...rest}
+    />
+  )
+}
+
+function Footer({
+  className,
+  srOnly = false,
+  ...rest
+}: ComponentProps<"p"> & {srOnly?: boolean}) {
+  return (
+    <p
+      className={cn(
+        "ListFooter",
+        "text-label-tertiary px-4 pt-1.5 text-sm leading-snug",
+        srOnly && "sr-only",
+        className
+      )}
+      {...rest}
+    />
+  )
+}
+
+const List = {Root, Item, Content, Icon, Text, Heading, Footer}
+export {List}
