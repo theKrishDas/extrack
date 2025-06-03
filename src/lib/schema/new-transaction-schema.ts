2402d9db76c5a65e89f603408ae7e3a8bae2025f
Krish-Das <krishdasatgames@gmail.com>
2025-06-03 19:42:33 +0530

    chore: add minimum length 1 in transaction note to prevent empty string

--- src/lib/schema/new-transaction-schema.ts
+++ src/lib/schema/new-transaction-schema.ts
@@ -17,7 +17,11 @@ export const newTransactionSchema = z.object({
     .max(MAXIMUM_TRANSACTION_AMOUNT, {
       message: `Amount must not exceed ${MAXIMUM_TRANSACTION_AMOUNT}.`,
     }),
-  note: z.string().max(MAX_NOTE_LENGTH).optional(),
+  note: z
+    .string()
+    .max(MAX_NOTE_LENGTH)
+    .min(1, {message: "Note can be undefined but not an empty string"})
+    .optional(),
   // category: z.string().nullable().optional(),
   type: z.union([z.literal("income"), z.literal("expense")], {
     message: "Type must be `income` or `expense`",
