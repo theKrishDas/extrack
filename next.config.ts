b796fdc0e11e6c42b403bc2ad259a80d171d5c85
Krish-Das <krishdasatgames@gmail.com>
2026-02-14 12:24:00 +0530

    chore(config): add allowedDevOrigins, devIndicators

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.1.*"],
+  devIndicators: { position: "top-right" },
 };
 
 export default nextConfig;
