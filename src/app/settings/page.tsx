a5a6b51da55ea7030327d1e5c8fc1ec74c6a9077
Krish-Das <krishdasatgames@gmail.com>
2026-03-28 14:34:28 +0530

    refactor(settings): use shared page header

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
 import { Container } from "@/components/layout/container"
-import { SettingsNav } from "./local-comps/nav"
+import { PageHeader } from "@/components/navigation/page-header"
 import { OptionList } from "./local-comps/SettingsOptions"
 
 export default function Page() {
   return (
     <>
-      <SettingsNav heading="Settings" href="/" />
+      <PageHeader backHref="/" title="Settings" />
       <main className="flex-1 px-4" data-vaul-drawer-wrapper="">
         <Container as="section" className="flex flex-col">
           <OptionList />
