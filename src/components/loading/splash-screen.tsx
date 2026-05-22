18924dbb415d0d17b3196372b879981dd6a0ea99
Krish-Das <krishdasatgames@gmail.com>
2026-05-22 08:45:59 +0000

    chore: use unicode ellipsis in loading text

--- src/components/loading/splash-screen.tsx
+++ src/components/loading/splash-screen.tsx
@@ -9,7 +9,7 @@ export function SplashScreen() {
       >
         <AppIcon />
       </span>
-      <span className="sr-only">Loading...</span>
+      <span className="sr-only">Loading…</span>
     </div>
   )
 }
